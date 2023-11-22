4e316c3f04681241d66c7911ac9e8668f0df3f37
Nathan <nathanlee3296@hotmail.co.uk>
2023-11-22 09:38:03 +0000

    Task 6

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const {
 	handlePsqlErrors,
 } = require("./controllers/errors.controller");
 const { getArticlesById } = require("./controllers/articles.controller");
+const { getCommentsByArticleID } = require("./controllers/comments.controller");
 
 const app = express();
 app.use(express.json());
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_Id", getArticlesById);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleID)
 
 app.all("/*", handleWrongURLS);
 app.use(handlePsqlErrors);
---
+++ models/api.comments.js
@@ -0,0 +1,15 @@
+const connection = require("../db/connection");
+
+exports.selectCommentsByID = ({ article_id }) => {
+	return connection
+		.query(
+			"SELECT * FROM comments WHERE article_id=$1 ORDER BY created_at DESC ",
+			[article_id]
+		)
+		.then(({ rows }) => {
+			if (rows.length === 0) {
+				return Promise.reject({ status: 404, msg: "Wrong Input" });
+			}
+			return rows;
+		});
+};
